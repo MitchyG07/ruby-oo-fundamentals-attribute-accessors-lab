03e4d1cdfe1feac7d4565d9201702d5cd28bdc27
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-11-17 21:57:41 -0800

    commit tree 2dc349d13fb81f0c7d0bae5b2d3a879c6f2a2cc6

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,17 @@
-## code your solution here. 
+class Cat
+    attr_accessor :name, :meow
+
+    def initialize()
+        @name = name 
+        @meow = "meow!"
+    end
+
+    def meow
+        puts @meow 
+    end 
+
+end
+
+ye = Cat.new()
+puts ye.name = "Yeezy"
+ye.meow
